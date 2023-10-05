3611b72e462353d6cb7a50d88b13dbd6faed2668
Fernando <fernandopereira-3@hotmail.com>
2023-10-05 16:49:07 -0300

    Node server

--- Prod/installansible.sh
+++ Prod/installansible.sh
@@ -3,34 +3,20 @@ cd /home/
 curl  https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
 sudo python3 -m pip install ansible 
-tee -a playbook.yml > /dev/null <<EOT
+tee -a playbook.yml > /home/ <<EOT
 - hosts: localhost
   tasks:
-  - name: Instalando o python3, virtualenv
-    apt:
+  - name: Instalando o NODE
+    snap
       pkg:
-      - python3
-      - virtualenv
+      - node
       update_cache: yes
     become: yes
-  - name: Instalando dependencias com pip (Django e Django Rest)
-    pip:
-      virtualenv: /home/tcc/venv
-      name:
-        - django
-        - djangorestframework
-  - name: Verificando se o projeto ja existe
-    stat:
-      path: /home/tcc/setup/settings.py
-    register: projeto
-  - name: Iniciando o projeto
-    shell: '. /home/tcc/venv/bin/activate; django-admin startproject setup /home/tcc/'
-    when: not projeto.stat.exists
-  - name: Alterando o hosts do settings
-    lineinfile:
-      path: /home/tcc/setup/settings.py
-      regexp: 'ALLOWED_HOSTS'
-      line: 'ALLOWED_HOSTS = ["*"]'
-      backrefs: yes
+  - name: Baixando git
+    ansible.builtin.git:
+    repo: 'https://github.com/fernandopereira3/node_server.git'
+    dest: /home/app
+  - name: Executando o server
+    shell: 'cd /home/app; sudo node server.js'
 EOT
 ansible-plabook playbook.yml
